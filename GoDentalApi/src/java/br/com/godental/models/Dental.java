55802838ce2a8e8d9f30dc2c9996ec45f1b23302
hudsonschumaker <hudsonschumaker@192.168.0.102>
2018-03-27 21:31:51 -0300

    Entities : User, Dental

---
+++ GoDentalApi/src/java/br/com/godental/models/Dental.java
@@ -0,0 +1,47 @@
+package br.com.godental.models;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author hudsonschumaker
+ */
+@Entity
+public class Dental implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private String name;
+    private String Adress;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }    
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAdress() {
+        return Adress;
+    }
+
+    public void setAdress(String Adress) {
+        this.Adress = Adress;
+    }
+}
---
+++ GoDentalApi/src/java/br/com/godental/models/User.java
@@ -0,0 +1,56 @@
+package br.com.godental.models;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author hudsonschumaker
+ */
+@Entity
+@Table(name = "Usuario")
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private String name;
+    private String email;
+    
+    public User(){
+    }
+
+    public User(Long id) {
+        this.id = id;
+    }
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
